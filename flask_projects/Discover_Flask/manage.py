d62dc14ee9f413723a41c6970b44152e64356c29
achicha <meth787@gmail.com>
2016-09-04 16:59:29 +0300

    add coverage

--- flask_projects/Discover_Flask/manage.py
+++ flask_projects/Discover_Flask/manage.py
@@ -1,6 +1,10 @@
+import os
+import unittest
+import coverage
+
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import app, db
+from project import app, db
 
 """
 how to use migrations:
@@ -14,6 +18,30 @@ migrate = Migrate(app, db)  # create migration instance
 manager = Manager(app)      # create Manager instance
 
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """Runs the tests without coverage."""
+    tests = unittest.TestLoader().discover('.')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    cov = coverage.coverage(branch=True, include='project/*')
+    cov.start()
+    tests = unittest.TestLoader().discover('.')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    cov.stop()
+    cov.save()
+    print('Coverage Summary:')
+    cov.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir, 'coverage')
+    cov.html_report(directory=covdir)
+    cov.erase()
 
 if __name__ == '__main__':
     manager.run()
